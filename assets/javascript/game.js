f9865da598eaaf948a6047eb494d004b554d2f2c
Jim M <Jim.R.Maloney@gmail.com>
2018-06-14 20:05:55 -0400

    updating Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,37 @@
 
-var letters = ["a","b","g","h","r","t","v","e","s","t"];
+// var letters = ["a","b","g","h","r","t","v","e","s","t"];
+var letters = ["a","b"];
 var wins = 0;
 var losses = 0;
-var left =5;
+var left =9;
 var GuessList = " " ;
 
 document.onkeyup = function(event) {
     var Guess = event.key;
     var computerPicks = letters[Math.floor(Math.random() * letters.length)];
-    GuessList = Guess + ","+ GuessList;
+    if (Guess != computerPicks) {    
+        left = left - 1; 
+        GuessList = GuessList +  Guess +",";
+    }
+    if (left === 0){
+        GuessList = "";
+        losses = losses + 1;
+        left=9;
+    }
+
+    if (Guess === computerPicks) {
+        wins = wins + 1;
+        GuessList = "";
+        left = 9 ;
+    }
+
+
 var html = "<p>Guess what letter I am thinking of</p>" +
           "<p>Wins: " + wins + "</p>" +
           "<p>losses: " + losses + "</p>" +
           "<p>Guesses Left: " + left + "</p>" +
           "<p> Your Guesses So Far: " + GuessList;          
           document.querySelector("#game").innerHTML = html;
-
-if (Guess === computerPicks) {
-    wins = wins + 1; 
-    losses = 0;
-    left = 5;
-    Guesslist = "";
-}
-if (left === 0){
-    GuessList = "";
-    losses = losses + 1;
-    left=5;
-}
-if (Guess != computerPicks) {    
-    left = left - 1;  
-}
+         
+       
 }
